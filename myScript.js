579bfe8c4f9fca00a6edfc7ed09d5b03485c4d20
Quotz <andrej.kocevski.koc@gmail.com>
2022-10-19 11:27:03 -0400

    implemented dynamic grid making

---
+++ myScript.js
@@ -0,0 +1,46 @@
+let gridNumber = 16;
+
+function makeGrid(x) {
+
+	let gridPlan = x*x;
+
+	for (let i = 0; i < gridPlan; i++) {
+
+		let div = document.createElement('div');
+		div.classList.add('gridItem');
+		div.addEventListener('mouseover', () => {
+			div.classList.toggle('colored')
+			console.log('added event listener')
+		});
+		let gridContainer = document.querySelector('.gridContainer');
+		gridContainer.appendChild(div);
+		console.log('grid item added');
+		
+	}
+}
+
+makeGrid(gridNumber);
+
+
+/*const gridItems = document.querySelectorAll('.gridItem');
+gridItems.forEach((item) => {
+	item.addEventListener('mouseover', () => {
+		item.classList.toggle('colored')
+	});
+});
+*/
+
+function removeGrid() {
+	let gridContainer = document.querySelector('.gridContainer');
+	while (gridContainer.firstChild) {
+  		gridContainer.removeChild(gridContainer.firstChild);
+	}
+}
+
+const button = document.querySelector('#gridNumberInput')
+button.addEventListener('click', () => {
+	let userInput = prompt('How many grid items?');
+	gridNumber = userInput;
+	removeGrid();
+	makeGrid(gridNumber);
+});
